9783b3fab709a4e0fc20216ebe92874e49992ea8
Jeremy FELIX <jeremy.felix971@gmail.com>
2018-10-18 22:35:46 -0400

    Extract index line OK!

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
-#include "image.h" 
+#include "image.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-int first_ligne;
+int  nb_cases=100;
+int debut_ligne[100];
+int fin_ligne[100];
+int debut_caract[100];
+int fin_caract[100];
+
 //------------------------------------------------------------------------------
 // Code source pour le projet d'UE035
-// description : (les fonctions sont définit dans image.h)
+// description : (les fonctions sont dï¿½finit dans image.h)
 //
 // les pointeurs images dans ce code sont de type : struct fichierimage *
 //
@@ -15,29 +20,29 @@ int first_ligne;
 // renvoie un pointeur de type : struct fichierimage *
 //
 // fonction int enregistrer(struct fichierimage *,char *)
-// permet d'enregistrer une image sur le disque sous le nom donné en arg2, cette 
+// permet d'enregistrer une image sur le disque sous le nom donnï¿½ en arg2, cette
 // image est contenue dans une pointeur de type : struct fichierimage * fournit en arg1
 //
 // fonction struct fichierimage * nouveau(int,int)
-// permet de creer une image en memoire de largeur arg1 et de hauteur arg2, la fonction 
+// permet de creer une image en memoire de largeur arg1 et de hauteur arg2, la fonction
 // retourne un pointeur de type : struct fichierimage *
 //------------------------------------------------------------------------------
 int debut_text(struct fichierimage *fichier)
 {
-	int i, j;	
+	int i, j;
 	for(j=0;j<fichier->entetebmp.largeur; j++)
 		for(i=0;i<fichier->entetebmp.hauteur; i++)
 			if(fichier->image[i][j].r == 255)
 				return j;
-	
+
 }
-/*Permet de mettre l'image en niv gris*/ 
+/*Permet de mettre l'image en niv gris*/
 void niv_gris(struct fichierimage *fichier)
 {
 	int nv_gris=0;
 	int i,j;
 	for(i=fichier->entetebmp.hauteur;i>=0;i--)
-	{    	
+	{
 		for(j=0;j<fichier->entetebmp.largeur;j++)
 		{
 		    nv_gris=(fichier->image[i][j].r + fichier->image[i][j].g + fichier->image[i][j].b)/3;
@@ -55,7 +60,7 @@ void inv_contraste(struct fichierimage *fichier)
 {
 	int i, j;
 	int seuil=128,nbpix=0;
-	
+
 	for(j=0; j<fichier->entetebmp.hauteur; j++)
 	{
 		for(i=0; i<fichier->entetebmp.largeur; i++)
@@ -79,61 +84,69 @@ void inv_contraste(struct fichierimage *fichier)
 	supprimer(fichier);
 }
 
-void extract_ligne(struct fichierimage *fichier)
+void extract_index_ligne(struct fichierimage *fichier)
 {
 	int i, j;
-	int stop=0,debut=0;
+	int index_line=0;
+	int fulline[fichier->entetebmp.hauteur];
 
-	//struct fichierimage *text[]= NULL;
-	struct fichierimage *ligne = NULL;
-	ligne=nouveau(fichier->entetebmp.largeur,fichier->entetebmp.hauteur); 
+	//Init de full line
+	for(i=0;i<fichier->entetebmp.hauteur;i++)
+		fulline[i]=0;
 
-	debut=debut_text(fichier);
-	printf("debut text : %d",debut);
-	for(j=debut; j<fichier->entetebmp.hauteur; j++)
+	//On parcours limage et si cest allume on incremente full line pour determiner les lignes en j
+	for(j=0;j<fichier->entetebmp.hauteur; j++)
 	{
-		for(i=0; i<fichier->entetebmp.largeur; i++)
+		for(i=0;i<fichier->entetebmp.largeur;i++)
 		{
-			if(fichier->image[i][j].r==255)
+			if(fichier->image[i][j].r > 0)
 			{
-				ligne->image[i][j].r=fichier->image[i][j].r;
-				ligne->image[i][j].g=fichier->image[i][j].g;
-				ligne->image[i][j].b=fichier->image[i][j].b;
+				fulline[j]+=1;
 			}
-			else
-			{
-				stop++;
-			}
-		}
-		if(stop==fichier->entetebmp.largeur)
-		{
-			printf("ici stop = %d j:%d\n",stop,j);
-			enregistrer("image_ligne1.bmp",ligne);
-			supprimer(ligne);
-			printf("j = %d\n",j);
-			return;
-		}
-		else
-		{
-			stop=0;
 		}
 	}
 
+	/*for(j=0;j<fichier->entetebmp.hauteur;j++)
+	{
+		printf(" fulline : %d  index : %d\n",fulline[j],j);
+	}*/
+
+	for(j=0;j<fichier->entetebmp.hauteur; j++)
+	{
+		if(fulline[j] > 0 && fulline[j-1]==0)
+		{
+			debut_ligne[index_line]=j;
+			printf("debut_ligne = %d  index = %d ",debut_ligne[index_line],index_line);
+		}
+		else if(fulline[j]>0 && fulline[j+1]==0)
+		{
+			fin_ligne[index_line]=j;
+			printf("fin_ligne = %d  index = %d \n",fin_ligne[index_line],index_line);
+			index_line+=1;
+		}
+	}
 }
 
 int main()
 {
+	int i;
+	for(i=0;i<nb_cases;i++)
+	{
+		debut_ligne[i]=0;
+		fin_ligne[i]=0;
+		debut_caract[i]=0;
+		fin_ligne[i]=0;
+	}
+	// exemple de dï¿½claration d'un pointeur image
+	struct fichierimage *fichier=NULL;
 
-// exemple de déclaration d'un pointeur image
-struct fichierimage *fichier=NULL;
+	//1- Inversion des Contrastes  de l'image et binarisation de l'image
 
-//1- Inversion des Contrastes  de l'image et binarisation de l'image
+	fichier = charger ("Document1.bmp");
 
-fichier = charger ("Document1.bmp");
+	inv_contraste(fichier);
 
-inv_contraste(fichier);
-
-fichier = charger ("image_binaire.bmp");
-extract_ligne(fichier);
+	fichier = charger ("image_binaire.bmp");
+	extract_index_ligne(fichier);
 
 }
